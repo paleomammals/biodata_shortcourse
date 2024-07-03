8335a3a98ffd0515e32ac1474deeb32a43d959cb
vjpsyverson <vsyverson@gmail.com>
2024-07-03 15:16:49 -0700

    fix longitude problem

--- assemble-paleoclimate.R
+++ assemble-paleoclimate.R
@@ -1,17 +1,18 @@
 #assemble climate layers into rasterstacks
-require(raster)
+require(raster); require(rbioclim)
 #Last glacial maximum, 22ka
-filenames<-list.files("climatelayers/lgm/",full.names=T)
-lgm<-stack(filenames)
+#filenames <- list.files("climatelayers/lgm/",full.names = T)
+filenames <- getData("worldclim_past", var = "bio", res = 2.5, past = "lgm")
+lgm <- stack(filenames)
 #Mid-Holocene, 6ka
-filenames<-list.files("climatelayers/midH/",full.names=T)
-midH<-stack(filenames)
+filenames <- list.files("climatelayers/midH/",full.names = T)
+midH <- stack(filenames)
 #Modern, 1960-2000CE
-modern<-getData('worldclim',var='bio',res=2.5)
+modern <- getData('worldclim',var = 'bio',res = 2.5)
 #Future, 2070CE
-filenames<-list.files("climatelayers/future2070/",full.names=T)
-future<-stack(filenames)
-save(lgm,midH,modern,future,file="climatelayers.RData")
+filenames <- list.files("climatelayers/future2070/",full.names = T)
+future <- stack(filenames)
+save(lgm,midH,modern,future,file = "climatelayers.RData")
 
 
 
--- installscript.R
+++ installscript.R
@@ -1,5 +1,15 @@
 install.packages("dismo")
-install.packages("maptools")
+install.packages("maps")
 install.packages("sf")
 install.packages("concaveman")
+install.packages("geodata")
 install.packages("raster")
+install.packages("here")
+install.packages("remotes")
+remotes::install_github("MoisesExpositoAlonso/rbioclim")
+transformlong <- function(longitude) {
+  result <- longitude
+  k <- which(result < 0)
+  result[k] <- 360 + result[k]
+  return(result)
+}
